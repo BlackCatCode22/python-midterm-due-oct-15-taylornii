80d0ca8dec7946225938b8ed8e4c82ece9573d42
taylornii <141867855+taylornii@users.noreply.github.com>
2023-10-15 19:17:01 -0700

    Update PythonMidterm.py

--- PythonMidterm.py
+++ PythonMidterm.py
@@ -13,6 +13,7 @@ today_str = f"{today['year']}-{today['month']}-{today['day']}"
 class Animal:
     # Static class variable to keep track of the number of animals
     numOfAnimals = 0
+    NUM_OF_ANIMALS = 0
     NUM_OF_HYENAS = 0
     NUM_OF_LIONS = 0
     NUM_OF_BEARS = 0
@@ -44,12 +45,22 @@ class Animal:
         self.birthdate = birthdate
         self.unique_id = unique_id
         self.sex = sex
-        self.infostring = ""  # a string formatted for zooPopulation, will eventually be appended to correct animal habitat list
 
         # Increment the static variable when a new object is created
         # this is the only place this field's value should be changed
         Animal.numOfAnimals += 1
 
+
+    def __str__(self):
+        print_str = f"{self.unique_id}; {self.name}; {self.age} years old; birth date {self.birthdate}; {self.color}; {self.sex}; {self.weight}; {self.originating_zoo}; arrived {today_str}"
+        return print_str
+
+
+    def __eq__(self, other):
+        if self.species == other.species:
+            return True
+        else:
+            return False
 
     def genBirthDay(self):
         """takes object season and age -> assigns randomly generated birthday to the object"""
@@ -79,14 +90,14 @@ class Animal:
             """takes randomly generated month -> returns randomly generated birth day"""
             months_31days = [1, 3, 5, 7, 8, 10, 12]  # jan, mar, may, jul, aug, oct, dec
             months_30days = [4, 6, 9, 11]  # apr, jun, sep, nov
-            months_29days = [2]  # feb
+            months_28days = [2]  # feb
             # return a random day from the assigned birth month
             if months_31days.count(month) == 1:
                 return random.choice(range(1, 32))
             elif months_30days.count(month) == 1:
                 return random.choice(range(1, 31))
-            elif months_29days.count(month) == 1:
-                return random.choice(range(1, 30))
+            elif months_28days.count(month) == 1:
+                return random.choice(range(1, 29))
             else:
                 return 0
 
@@ -123,97 +134,119 @@ class Animal:
     # TODO: where do you apply 4 animal max cut-off?
     def genUniqueAnimalID(self):
         """generates unique id from animal object & assigns to member variable, unique_id"""
-        animal_type = self.species
-        if animal_type == "lion":
+        if self == Lion():
             Animal.NUM_OF_LIONS += 1
-            self.secondpt = "0"+str(Animal.NUM_OF_LIONS)
-        elif animal_type == "hyena":
+            Animal.NUM_OF_ANIMALS +=1
+            id_num = "0"+str(Animal.NUM_OF_LIONS)
+        elif self == Hyena():
             Animal.NUM_OF_HYENAS += 1
-            self.secondpt = "0"+str(Animal.NUM_OF_HYENAS)
-        elif animal_type == "bear":
+            Animal.NUM_OF_ANIMALS += 1
+            id_num = "0"+str(Animal.NUM_OF_HYENAS)
+        elif self == Bear():
             Animal.NUM_OF_BEARS += 1
-            self.secondpt = "0" + str(Animal.NUM_OF_BEARS)
-        elif animal_type == "tiger":
+            Animal.NUM_OF_ANIMALS += 1
+            id_num = "0" + str(Animal.NUM_OF_BEARS)
+        elif self == Tiger():
             Animal.NUM_OF_TIGERS += 1
-            self.secondpt = "0" + str(Animal.NUM_OF_TIGERS)
-        self.firstpt = self.species[0:2].title()
-        unique_id = self.firstpt + self.secondpt
+            Animal.NUM_OF_ANIMALS += 1
+            id_num = "0" + str(Animal.NUM_OF_TIGERS)
+        else:
+            id_num = "00"
 
+        id_alpha = self.species[0:2].title()
+        unique_id = id_alpha + id_num
         self.unique_id = unique_id
 
 
     def genAnimalName(self):
-        if self.species == "hyena":
+        if self == Hyena():
             picked = random.choice(Animal.hyena_names)
             Animal.hyena_names.remove(picked)
             self.name = picked
-        elif self.species == "lion":
+        elif self == Lion():
             picked = random.choice(Animal.lion_names)
             Animal.lion_names.remove(picked)
             self.name = picked
-        elif self.species == "bear":
+        elif self == Bear():
             picked = random.choice(Animal.bear_names)
             Animal.bear_names.remove(picked)
             self.name = picked
-        elif self.species == "tiger":
+        elif self == Tiger():
             picked = random.choice(Animal.tiger_names)
             Animal.tiger_names.remove(picked)
             self.name = picked
-        self.infostring = f"{self.unique_id}; {self.name}; {self.age} years old; birth date {self.birthdate}; {self.color}; {self.sex}; {self.weight}; {self.originating_zoo}; arrived {today_str}"
 
-    # Hy01; Banzai; 4 years old; birth date 2019-03-21; tan color; female; 70 pounds; from Friguia Park, Tunisia; arrived 2023-09-14
 
     def genZooHabitat(self):
-        """should take in animal object, append them to as list if the list isn't full aka len of list is < 4"""
-        """then should put those 4 lists into zoo_habitat list """
-        # while len(Animal.hyena_habitat) + len(Animal.lion_habitat) + len(Animal.bear_habitat) + len(Animal.tiger_habitat) < 16:
-            # Maybe add NUM_OF_ANIMALS += 1 here?? TODO
-        if self.species == "hyena":
-            if len(Animal.hyena_habitat) < Animal.HABITAT_CAPACITY:
-                self.genBirthDay()
-                self.genUniqueAnimalID()
-                self.genAnimalName()
-                Animal.hyena_habitat.append(self.infostring)
-                # MAYBE add NUM_OF_HYENAS += 1 here??  TODO
-        elif self.species == "lion":
-            if len(Animal.lion_habitat) < Animal.HABITAT_CAPACITY:
-                self.genBirthDay()
-                self.genUniqueAnimalID()
-                self.genAnimalName()
-                Animal.lion_habitat.append(self.infostring)
-        #         TODO
-        elif self.species == "bear":
-            if len(Animal.bear_habitat) < Animal.HABITAT_CAPACITY:
-                self.genBirthDay()
-                self.genUniqueAnimalID()
-                self.genAnimalName()
-                Animal.bear_habitat.append(self.infostring)
-        #         TODO
-        elif self.species == "tiger":
-            if len(Animal.tiger_habitat) < Animal.HABITAT_CAPACITY:
-                self.genBirthDay()
-                self.genUniqueAnimalID()
-                self.genAnimalName()
-                Animal.tiger_habitat.append(self.infostring)
-    #             TODO
+        """should take in animal object, append them to a habitat list if the habitat isn't full aka len of list is < 4"""
+        if len(Animal.hyena_habitat) + len(Animal.lion_habitat) + len(Animal.bear_habitat) + len(Animal.tiger_habitat) < 4*Animal.HABITAT_CAPACITY:
+            if self == Hyena():
+                if len(Animal.hyena_habitat) < Animal.HABITAT_CAPACITY:
+                    self.genBirthDay()
+                    self.genUniqueAnimalID()
+                    self.genAnimalName()
+                    Animal.hyena_habitat.append(str(self))
+            elif self == Lion():
+                if len(Animal.lion_habitat) < Animal.HABITAT_CAPACITY:
+                    self.genBirthDay()
+                    self.genUniqueAnimalID()
+                    self.genAnimalName()
+                    Animal.lion_habitat.append(str(self))
+            elif self == Bear():
+                if len(Animal.bear_habitat) < Animal.HABITAT_CAPACITY:
+                    self.genBirthDay()
+                    self.genUniqueAnimalID()
+                    self.genAnimalName()
+                    Animal.bear_habitat.append(str(self))
+            elif self == Tiger():
+                if len(Animal.tiger_habitat) < Animal.HABITAT_CAPACITY:
+                    self.genBirthDay()
+                    self.genUniqueAnimalID()
+                    self.genAnimalName()
+                    Animal.tiger_habitat.append(str(self))
     zoo_habitat = [hyena_habitat, lion_habitat, bear_habitat, tiger_habitat]
+
+
+class Hyena(Animal):
+    def __init__(self, name=None, age=99, birth_season="season", color="a color", weight="99 pounds",
+                 originating_zoo="from where?", birthdate="", unique_id=None, sex=""):
+        super().__init__(species="hyena")
+
+
+class Lion(Animal):
+    def __init__(self, name=None, age=99, birth_season="season", color="a color", weight="99 pounds",
+                 originating_zoo="from where?", birthdate="", unique_id=None, sex=""):
+        super().__init__(species="lion")
+
+
+class Bear(Animal):
+    def __init__(self, name=None, age=99, birth_season="season", color="a color", weight="99 pounds",
+                 originating_zoo="from where?", birthdate="", unique_id=None, sex=""):
+        super().__init__(species="bear")
+
+class Tiger(Animal):
+    def __init__(self, name=None, age=99, birth_season="season", color="a color", weight="99 pounds",
+                 originating_zoo="from where?", birthdate="", unique_id=None, sex=""):
+        super().__init__(species="tiger")
+
 
 
 fileipath_animalNames = "/Users/taylornii/PycharmProjects/CIT95Programs/PythonMidterm/animalNames.txt"
 fileipath_arrivingAnimals = "/Users/taylornii/PycharmProjects/CIT95Programs/PythonMidterm/arrivingAnimals.txt"
 fileopath_outputFile = "/Users/taylornii/PycharmProjects/CIT95Programs/PythonMidterm/outputFile.txt"
+
 try:
     with open(fileipath_animalNames, 'r') as animalNames:
-        # Read lines from the input file
+        """reading in animalNames, creating list of names by respective species"""
         a_lines = animalNames.readlines()
-        b_lines = []
+        a_list = []
         for line in a_lines:
             line = line.strip()
             if line != "":
-                b_lines.append(line)
+                a_list.append(line)
 
         element_index = 0
-        for element in b_lines:
+        for element in a_list:
             if element == "Hyena Names:":
                 hyena_index = element_index + 1
             elif element == "Lion Names:":
@@ -223,14 +256,15 @@ try:
             elif element == "Tiger Names:":
                 tiger_index = element_index + 1
             element_index += 1
-        Animal.hyena_names = b_lines[hyena_index].split(", ")
-        Animal.lion_names = b_lines[lion_index].split(", ")
-        Animal.bear_names = b_lines[bear_index].split(", ")
-        Animal.tiger_names = b_lines[tiger_index].split(", ")
+        Animal.hyena_names = a_list[hyena_index].split(", ")
+        Animal.lion_names = a_list[lion_index].split(", ")
+        Animal.bear_names = a_list[bear_index].split(", ")
+        Animal.tiger_names = a_list[tiger_index].split(", ")
 
 
     with (open(fileipath_arrivingAnimals, 'r') as arrivingAnimals):
-        # Read lines from the input file
+        """reading in arrivingAnimals, make individual animal an object & assigning their attributes, 
+        and placing them in their habitat list by calling genZooHabitat()"""
         b1_lines = arrivingAnimals.readlines()
         b2_lines = []
         b3_lines = []
@@ -243,14 +277,14 @@ try:
             line = line.replace(" year old", ",").replace("male", "male,")
             line = line.replace("born in ", "").replace(", ", ";", 6)
             animalx_list = line.split(";")
-            animalx = Animal(animalx_list[2])
+            animalx = Animal(animalx_list[2])  # creating animalx object in Animal class
+            # assigning attributes from item in animalx_list containing qualities read in from arrivingAnimals
             animalx.age = int(animalx_list[0])
             animalx.birth_season = animalx_list[3]
             animalx.color = animalx_list[4]
             animalx.weight = animalx_list[5]
             animalx.originating_zoo = animalx_list[6]
             animalx.sex = animalx_list[1]
-            # age=99, birth_season="season", color="a color", weight="99 pounds", originating_zoo="from where?", birthdate="", unique_id=None, sex=""
             animalx.genZooHabitat()
 
         with open(fileopath_outputFile, "w") as outputFile:
@@ -272,3 +306,4 @@ try:
                 outputFile.write("\n")
 except Exception as e:
     print(f"An error occurred: {str(e)}")
+    
